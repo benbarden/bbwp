fe905439d0aa185ad4c175ff4e73abf263cf5fea
Ben Barden <628824+benbarden@users.noreply.github.com>
2021-09-19 21:14:31 +0100

    First attempt with Timber (still a WIP)

---
+++ wp-content/themes/bb/sidebar.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * The Template for the sidebar containing the main widget area
+ *
+ * @package  WordPress
+ * @subpackage  Timber
+ */
+
+$context = array();
+$recentPostsArgs = ['numberposts' => 5];
+$context['RecentPosts'] = new Timber\PostQuery($recentPostsArgs);
+Timber::render( array( 'sidebar.twig' ), $context );
